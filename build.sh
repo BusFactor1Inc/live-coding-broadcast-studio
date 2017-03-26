880aab8c6aea5d204a0d066996ec6f1db3804282
BusFactor1 Inc <BusFactor1@gmai.com>
2017-03-25 22:37:55 -0600

    Updated build.sh.

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
-[[ ! -z $1 ]] && rm -rf build && mkdir build
+version=${1?version?}
+name="${2-Live Coding Broadcast Studio (LCBS)}"
+
+[[ ! -z $3 ]] && rm -rf build && mkdir build
 
 cd build && 
-    cmake .. && 
-    make -j17 package && 
-# TODO: remove old .app
-    cp -a "$(list -type d -name '*.app')" ~ &&
-    cd ..
+    gsed "s|LCBS_VERSION_NUMBER|${version}|" ../cmake/osxbundle/Info.plist.in > ../cmake/osxbundle/Info.plist &&
+    cmake .. &&
+    make -j17 package 
+    # TODO: remove old .app
+
+app="$(list -type d -name "${name}.app")"
+echo App: "${app}"
+
+if [[ -z "${app}" ]]; then exit 1; fi
+
+rm -rf ../"${name}.app"
+cp -a "${app}" ..
+cd .. &&
+    ./sign.sh &&
+    cp -a build/"${app}" "${name}.app,${version}"
+
